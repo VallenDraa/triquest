d90a0dceedd47566fed9a1135937d2b46e56f56a
KoooYoung <Jestinevallendradwiputra@gmail.com>
2022-01-24 21:43:22 +0700

    reconfigure the question card size and also added monitoring for the length of the questions strings

--- public/gameplay_js/gameplay.js
+++ public/gameplay_js/gameplay.js
@@ -1,4 +1,4 @@
-const questionCard = document.querySelector('.question-card');
+const questionCardWrapper = document.querySelector('.question-card-wrapper');
 
 // calling api and assigning the questions
 const questions = {
@@ -12,15 +12,19 @@ const questions = {
   assignQuestion: (questionsArr) => {
     questionsArr.forEach((question, i) => {
       if (question.type === 'multiple') {
-        let htmlEl = ` <div
-        class="bg-yellow-300 border-4 border-black rounded-xl shadow-2xl px-5 md:px-10 py-5 my-20 four-option "
+        let htmlEl = ` 
+        <!-- four opt -->
+        <div
+        class="bg-yellow-300 border-4 border-black question-card rounded-xl shadow-2xl px-2 md:px-10 py-2 md:py-5 my-20 md:my-20  md:h-5/6 four-option flex justify-center items-center"
       >
         <div
-          class="fira-sans space-y-3 border-4 border-black rounded-lg bg-yellow-200 p-4"
+          class="fira-sans p-4 w-full"
         >
           <section>
-            <h2 class="text-2xl md:text-3xl font-bold text-center py-4 max-w-md text-center mx-auto">
-              ${question.question}
+            <h2 class="text-xl md:text-3xl font-bold text-center py-4 max-w-4xl text-center mx-auto question" style="${questions.checkHowLongQuestion(
+              question.question
+            )}">
+            ${question.question}
             </h2>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
               ${questions
@@ -34,7 +38,7 @@ const questions = {
             <section class="my-4 h-6 border-4 border-black rounded-xl bg-slate-200">
               <div class="h-full  duration-1000 bg-green-400 rounded-xl  timer-bar"></div>
             </section>
-            <div class="flex justify-center items-center pt-4 font-light max-w-md mx-auto text-center">
+            <div class="flex justify-center items-center pt-4 text-xs font-light mx-auto text-center">
               <p class="text-center">Questions ${i + 1}/${
           questionsArr.length
         } · ${question.difficulty} · ${question.category}</p>
@@ -43,7 +47,7 @@ const questions = {
         </div>
       </div>
       `;
-        questionCard.innerHTML += htmlEl;
+        questionCardWrapper.innerHTML += htmlEl;
       } else {
         let identifierTrue, identifierFalse;
         const trueOpt = 'True';
@@ -59,23 +63,23 @@ const questions = {
         let htmlEl = ` 
         <!-- bool option -->
         <div
-          class="bg-yellow-300 border-4 border-black rounded-xl shadow-2xl px-5 md:px-10 py-5 my-20  four-option"
+          class="bg-yellow-300 border-4 border-black question-card rounded-xl shadow-2xl px-2 md:px-10 py-2 md:py-5 my-12 md:my-20 h-5/6 flex justify-center items-center bool-option"
         >
           <div
-            class="fira-sans space-y-3 border-4 border-black rounded-lg bg-yellow-200 p-4"
+            class="fira-sans p-4 w-full"
           >
             <section>
-              <h2 class="text-2xl md:text-3xl font-bold text-center py-4 max-w-md text-center mx-auto">
+              <h2 class="text-xl md:text-3xl font-bold text-center py-4 max-w-4xl text-center mx-auto question">
               ${question.question}
               </h2>
               <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
                 <div
-                  class="teko border-4 border-black text-3xl md:text-4xl h-24 md:h-44 flex justify-center items-center font-bold duration-300 bg-green-300 hover:text-4xl md:hover:text-5xl hover:bg-green-400 rounded-xl cursor-pointer ${identifierTrue}"
+                  class="teko border-4 border-black text-2xl md:text-4xl h-16 md:h-24 flex justify-center items-center font-bold duration-300 bg-green-300 hover:bg-green-400 rounded-xl cursor-pointer ${identifierTrue}"
                 >
                   True
                 </div>
                 <div
-                  class="teko border-4 border-black text-3xl md:text-4xl h-24 md:h-44 flex justify-center items-center font-bold duration-300 bg-red-300 hover:text-4xl md:hover:text-5xl hover:bg-red-400 rounded-xl cursor-pointer ${identifierFalse}"
+                  class="teko border-4 border-black text-2xl md:text-4xl h-16 md:h-24 flex justify-center items-center font-bold duration-300 bg-red-300 hover:bg-red-400 rounded-xl cursor-pointer ${identifierFalse}"
                 >
                   False
                 </div>
@@ -84,7 +88,7 @@ const questions = {
               <section class="my-4 h-6 border-4 border-black rounded-xl bg-slate-200">
                 <div class="h-full duration-1000 bg-green-400 rounded-xl timer-bar"></div>
               </section>
-              <div class="flex justify-center items-center pt-4 font-light max-w-md mx-auto text-center">
+              <div class="flex justify-center items-center pt-4 text-xs font-light mx-auto text-center">
                 <p class="text-center">Questions ${i + 1}/${
           questionsArr.length
         } · ${question.difficulty} · ${question.category}</p>
@@ -92,7 +96,7 @@ const questions = {
             </section>
           </div>
         </div>`;
-        questionCard.innerHTML += htmlEl;
+        questionCardWrapper.innerHTML += htmlEl;
       }
     });
   },
@@ -139,14 +143,14 @@ const questions = {
       // insert to html and check if the choice is the correct Answer
       if (choices[index] === correct) {
         let htmlEl = ` <div
-        class="teko border-4 text-center border-black text-3xl h-24 md:h-44 flex justify-center items-center font-bold duration-300 hover:text-4xl  rounded-xl cursor-pointer co ${bgColor} ${hoverBgColor}"
+        class="teko border-4 text-center border-black text-xl md:text-2xl h-16 md:h-24 flex justify-center items-center font-bold duration-300  rounded-xl cursor-pointer co ${bgColor} ${hoverBgColor}"
       >
         ${choices[index]}
       </div>`;
         result.push(htmlEl);
       } else {
         let htmlEl = ` <div
-        class="teko border-4 text-center border-black text-3xl h-24 md:h-44 flex justify-center items-center font-bold duration-300 hover:text-4xl  rounded-xl cursor-pointer in ${bgColor} ${hoverBgColor}"
+        class="teko border-4 text-center border-black text-xl md:text-2xl h-16 md:h-24 flex justify-center items-center font-bold duration-300  rounded-xl cursor-pointer in ${bgColor} ${hoverBgColor}"
       >
         ${choices[index]}
       </div>`;
@@ -156,6 +160,33 @@ const questions = {
     }
 
     return result;
+  },
+  checkHowLongQuestion: (question) => {
+    if (question.length > 100 && question.length < 130) {
+      if (window.innerWidth > 768) {
+        // console.log(
+        //   `succeed in changing font-size of question text for:\n\n \"${question}\"\n\ntype [1A]`
+        // );
+        return `font-size: 1.5rem; line-height: 2rem;`;
+      } else {
+        // console.log(
+        //   `succeed in changing font-size of question text for:\n\n \"${question}\"\n\ntype [1B]`
+        // );
+        return `font-size: 1.125rem; line-height: 1.75rem;`;
+      }
+    } else if (question.length > 130 && question.length < 160) {
+      if (window.innerWidth > 768) {
+        // console.log(
+        //   `succeed in changing font-size of question text for:\n\n \"${question}\"\n\ntype [2A]`
+        // );
+        return `font-size: 1.125rem; line-height: 1.75rem;`;
+      } else {
+        // console.log(
+        //   `succeed in changing font-size of question text for:\n\n \"${question}\"\n\ntype [2B]`
+        // );
+        return `font-size: 1rem; line-height: 1.5rem;`;
+      }
+    }
   },
 };
 questions.fetchQuestion();
@@ -186,3 +217,33 @@ setTimeout(() => {
     }
   }, 100);
 }, 1000);
+
+// resize the questions font size
+window.addEventListener('resize', () => {
+  document.querySelectorAll('.question').forEach((question) => {
+    question.setAttribute(
+      'style',
+      questions.checkHowLongQuestion(question.innerText)
+    );
+  });
+});
+
+// move question from bottom to top
+let answerOpt,
+  questionCard = document.querySelectorAll('.question-card');
+setTimeout(() => {
+  answerOpt = document.querySelector('.answer-option');
+}, 500);
+
+setInterval(() => {
+  if (answerOpt.getAttribute('style') == null) {
+    console.log(window.getComputedStyle(questionCard[0]));
+    answerOpt.setAttribute('style', `transform: translateY(-${$})`);
+  } else {
+    if (answerOpt.getAttribute('style').indexOf('transform') != -1) {
+      // alert('Not Empty');
+    } else {
+      // alert('Empty');
+    }
+  }
+}, 4000);
