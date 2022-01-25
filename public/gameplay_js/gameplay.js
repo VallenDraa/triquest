c14bbb47cf8a69a8991fe5e29b2f4545f2db1668
KoooYoung <Jestinevallendradwiputra@gmail.com>
2022-01-25 21:23:08 +0700

    did code refactoring fot the question and also added a temporary loading screen

--- public/gameplay_js/gameplay.js
+++ public/gameplay_js/gameplay.js
@@ -6,160 +6,99 @@ const questions = {
     return fetch('../data/sampleQuestion.json')
       .then((res) => res.json())
       .then((questionsRes) => {
-        questions.assignQuestion(questionsRes.results);
-      });
+        questionsRes.results.forEach((question) => {
+          questions.questionList.push(question);
+        });
+      })
+      .then(() =>
+        questions.assignQuestionPropertyToCard(
+          questions.questionList,
+          questions.currentQuestion
+        )
+      );
   },
-  assignQuestion: (questionsArr) => {
-    questionsArr.forEach((question, i) => {
-      if (question.type === 'multiple') {
-        let htmlEl = ` 
-        <!-- four opt -->
-        <div
-        class="bg-yellow-300 border-4 border-black question-card rounded-xl shadow-2xl px-2 md:px-10 py-2 md:py-5 my-20 md:my-20  md:h-5/6 four-option flex justify-center items-center"
-      >
-        <div
-          class="fira-sans p-4 w-full"
-        >
-          <section>
-            <h2 class="text-xl md:text-3xl font-bold text-center py-4 max-w-4xl text-center mx-auto question" style="${questions.checkHowLongQuestion(
-              question.question
-            )}">
-            ${question.question}
-            </h2>
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-              ${questions
-                .multipleAnswerAssign(
-                  question.correct_answer,
-                  question.incorrect_answers
-                )
-                .join('')}
-            </div>
-            <!-- timer bar -->
-            <section class="my-4 h-6 border-4 border-black rounded-xl bg-slate-200">
-              <div class="h-full  duration-1000 bg-green-400 rounded-xl  timer-bar"></div>
-            </section>
-            <div class="flex justify-center items-center pt-4 text-xs font-light mx-auto text-center">
-              <p class="text-center">Questions ${i + 1}/${
-          questionsArr.length
-        } · ${question.difficulty} · ${question.category}</p>
-            </div>
-          </section>
-        </div>
-      </div>
-      `;
-        questionCardWrapper.innerHTML += htmlEl;
-      } else {
-        let identifierTrue, identifierFalse;
-        const trueOpt = 'True';
-
-        if (trueOpt === question.correct_answer) {
-          identifierTrue = 'co';
-          identifierFalse = 'in';
-        } else {
-          identifierTrue = 'in';
-          identifierFalse = 'co';
-        }
-
-        let htmlEl = ` 
-        <!-- bool option -->
-        <div
-          class="bg-yellow-300 border-4 border-black question-card rounded-xl shadow-2xl px-2 md:px-10 py-2 md:py-5 my-12 md:my-20 h-5/6 flex justify-center items-center bool-option"
-        >
-          <div
-            class="fira-sans p-4 w-full"
-          >
-            <section>
-              <h2 class="text-xl md:text-3xl font-bold text-center py-4 max-w-4xl text-center mx-auto question">
-              ${question.question}
-              </h2>
-              <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-                <div
-                  class="teko border-4 border-black text-2xl md:text-4xl h-16 md:h-24 flex justify-center items-center font-bold duration-300 bg-green-300 hover:bg-green-400 rounded-xl cursor-pointer ${identifierTrue}"
-                >
-                  True
-                </div>
-                <div
-                  class="teko border-4 border-black text-2xl md:text-4xl h-16 md:h-24 flex justify-center items-center font-bold duration-300 bg-red-300 hover:bg-red-400 rounded-xl cursor-pointer ${identifierFalse}"
-                >
-                  False
-                </div>
-              </div>
-              <!-- timer bar -->
-              <section class="my-4 h-6 border-4 border-black rounded-xl bg-slate-200">
-                <div class="h-full duration-1000 bg-green-400 rounded-xl timer-bar"></div>
-              </section>
-              <div class="flex justify-center items-center pt-4 text-xs font-light mx-auto text-center">
-                <p class="text-center">Questions ${i + 1}/${
-          questionsArr.length
-        } · ${question.difficulty} · ${question.category}</p>
-              </div>
-            </section>
-          </div>
-        </div>`;
-        questionCardWrapper.innerHTML += htmlEl;
-      }
-    });
+  assignQuestionPropertyToCard: (questionsArr, i) => {
+    // for multiple choice
+    if (questionsArr[i].type == 'multiple') {
+      document.querySelector('.question-four-opt').innerHTML =
+        questionsArr[i].question;
+      questions.multipleAnswerAssign(
+        questionsArr[i].correct_answer,
+        questionsArr[i].incorrect_answers,
+        document.querySelectorAll('.answer-four')
+      );
+      questions.questionCardFooter(questionsArr, i);
+      document.querySelector('.question-card-two').classList.add('hidden');
+      document.querySelector('.question-card-four').classList.remove('hidden');
+    }
+    // for boolean choice
+    else {
+      document.querySelector('.question-two-opt').innerHTML =
+        questionsArr[i].question;
+      questions.boolAnswerAssign(
+        questionsArr[i].correct_answer,
+        document.querySelectorAll('.answer-two')
+      );
+      questions.questionCardFooter(questionsArr, i);
+      document.querySelector('.question-card-four').classList.add('hidden');
+      document.querySelector('.question-card-two').classList.remove('hidden');
+    }
   },
-  multipleAnswerAssign: (correct, incorrects) => {
+  multipleAnswerAssign: (correct, incorrects, element) => {
     let choices = incorrects.concat(correct),
-      pastNum = [],
-      index,
-      bgColor,
-      hoverBgColor,
-      colorIndex = 0;
-    const result = [];
+      pastNumChoice = [],
+      pastNumCard = [],
+      indexChoice,
+      indexCard;
+
+    function generateIndex(pastNumType, indexType) {
+      if (pastNumType.length != 0) {
+        while (pastNumType.includes(indexType)) {
+          indexType = Math.round(Math.random() * 3);
+        }
+      } else {
+        indexType = Math.round(Math.random() * 3);
+      }
+      return indexType;
+    }
 
     for (const choice in choices) {
       // randomize the choice input to html
-      if (pastNum.length != 0) {
-        while (pastNum.includes(index)) {
-          index = Math.round(Math.random() * 3);
-        }
-      } else {
-        index = Math.round(Math.random() * 3);
-      }
-
-      // change the bg-color
-      switch (colorIndex) {
-        case 0:
-          bgColor = `bg-orange-300`;
-          hoverBgColor = `hover:bg-orange-400`;
-          break;
-        case 1:
-          bgColor = `bg-red-300`;
-          hoverBgColor = `hover:bg-red-400`;
-          break;
-        case 2:
-          bgColor = `bg-blue-300`;
-          hoverBgColor = `hover:bg-blue-400`;
-          break;
-        case 3:
-          bgColor = `bg-green-300`;
-          hoverBgColor = `hover:bg-green-400`;
-          break;
-      }
-      colorIndex++;
+      indexChoice = generateIndex(pastNumChoice, indexChoice);
+      indexCard = generateIndex(pastNumCard, indexCard);
 
       // insert to html and check if the choice is the correct Answer
-      if (choices[index] === correct) {
-        let htmlEl = ` <div
-        class="teko border-4 text-center border-black text-xl md:text-2xl h-16 md:h-24 flex justify-center items-center font-bold duration-300  rounded-xl cursor-pointer co ${bgColor} ${hoverBgColor}"
-      >
-        ${choices[index]}
-      </div>`;
-        result.push(htmlEl);
-      } else {
-        let htmlEl = ` <div
-        class="teko border-4 text-center border-black text-xl md:text-2xl h-16 md:h-24 flex justify-center items-center font-bold duration-300  rounded-xl cursor-pointer in ${bgColor} ${hoverBgColor}"
-      >
-        ${choices[index]}
-      </div>`;
-        result.push(htmlEl);
+      if (choices[indexChoice] === correct) {
+        element[indexCard].classList.add('co');
       }
-      pastNum.push(index);
-    }
+      element[indexCard].textContent = choices[indexChoice];
 
-    return result;
+      //  pushing the index to the respective array, so that, that number index wont be picked again
+      pastNumChoice.push(indexChoice);
+      pastNumCard.push(indexCard);
+    }
+  },
+  boolAnswerAssign: (correct, answerCard) => {
+    const trueOpt = 'True';
+
+    if (trueOpt === correct) {
+      answerCard[0].classList.add('co');
+      answerCard[1].classList.add('li');
+    } else {
+      answerCard[0].classList.add('li');
+      answerCard[1].classList.add('co');
+    }
+  },
+  questionCardFooter: (questionsArr, i) => {
+    if (questionsArr[i].type == 'multiple') {
+      document.querySelector('.footer-four-opt').textContent = `${i + 1}/${
+        questionsArr.length
+      } · ${questionsArr[i].difficulty} · ${questionsArr[i].category}`;
+    } else {
+      document.querySelector('.footer-two-opt').textContent = `${i + 1}/${
+        questionsArr.length
+      } · ${questionsArr[i].difficulty} · ${questionsArr[i].category}`;
+    }
   },
   checkHowLongQuestion: (question) => {
     if (question.length > 100 && question.length < 130) {
@@ -188,35 +127,39 @@ const questions = {
       }
     }
   },
+  questionList: [],
+  currentQuestion: 0,
 };
 questions.fetchQuestion();
 
-// timerBar
-let time = 15;
-setTimeout(() => {
-  const timerBars = document.querySelectorAll('.timer-bar');
-  const timer = setInterval(() => {
-    time -= 0.1;
-  }, 100);
-  const changeBar = setInterval(() => {
-    timerBars.forEach((timerBar) => {
-      timerBar.setAttribute('style', `width:${(time * 10) / 1.5}%`);
-      if (time <= 10) {
-        timerBar.classList.replace('bg-green-400', 'bg-orange-400');
-      }
-      if (time <= 5) {
-        timerBar.classList.replace('bg-orange-400', 'bg-red-500');
-      }
+// timerBar function
+function runTimerBar() {
+  let time = 15;
+  setTimeout(() => {
+    const timerBars = document.querySelectorAll('.timer-bar');
+    const timer = setInterval(() => {
+      time -= 0.1;
+    }, 100);
+    const changeBar = setInterval(() => {
+      timerBars.forEach((timerBar) => {
+        timerBar.setAttribute('style', `width:${(time * 10) / 1.5}%`);
+        if (time <= 10) {
+          timerBar.classList.replace('bg-green-400', 'bg-orange-400');
+        }
+        if (time <= 5) {
+          timerBar.classList.replace('bg-orange-400', 'bg-red-500');
+        }
+        if (time <= 0.1) {
+          timerBar.classList.replace('bg-red-500', 'bg-slate-200');
+        }
+      });
       if (time <= 0.1) {
-        timerBar.classList.replace('bg-red-500', 'bg-slate-200');
+        clearInterval(changeBar);
+        clearInterval(timer);
       }
-    });
-    if (time <= 0.1) {
-      clearInterval(changeBar);
-      clearInterval(timer);
-    }
-  }, 100);
-}, 1000);
+    }, 100);
+  }, 1000);
+}
 
 // resize the questions font size
 window.addEventListener('resize', () => {
@@ -235,15 +178,34 @@ setTimeout(() => {
   answerOpt = document.querySelector('.answer-option');
 }, 500);
 
-setInterval(() => {
-  if (answerOpt.getAttribute('style') == null) {
-    console.log(window.getComputedStyle(questionCard[0]));
-    answerOpt.setAttribute('style', `transform: translateY(-${$})`);
-  } else {
-    if (answerOpt.getAttribute('style').indexOf('transform') != -1) {
-      // alert('Not Empty');
-    } else {
-      // alert('Empty');
+// everytime an answer is clicked
+document.querySelectorAll('.answer-opt').forEach((option) => {
+  option.addEventListener('click', () => {
+    // add the index
+    if (questions.currentQuestion < 9) {
+      questions.currentQuestion++;
+      console.log(questions.currentQuestion);
     }
+
+    // refresh the question
+    questions.assignQuestionPropertyToCard(
+      questions.questionList,
+      questions.currentQuestion
+    );
+  });
+});
+
+// loading screen
+if (document.readyState === 'loading') {
+  function removeLoadingScreen() {
+    document.querySelector('.loading-screen').classList.add('hidden');
   }
-}, 4000);
+
+  document.addEventListener('DOMContentLoaded', () => {
+    removeLoadingScreen();
+    runTimerBar();
+  });
+} else {
+  removeLoadingScreen();
+  runTimerBar();
+}
